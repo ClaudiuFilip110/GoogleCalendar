f08f376a3524a2309387ada1ce0ce096b30bb6a5
ClaudiuFilip110 <clawdelu.filip@gmail.com>
2020-03-06 00:04:51 +0200

    Add userInput posibilities
    simple UI tweaks
    remove reduntant code
    add posibility for user to see events for the current day

--- calendarSimpleExample.py
+++ calendarSimpleExample.py
@@ -11,6 +11,7 @@ import time
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+
 def main():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -36,66 +37,107 @@ def main():
     # Call the Calendar API
     calendarList = service.calendarList().list().execute()
 
-    #create an array for later use of calendarId
+    # create an array for later use of calendarId
     calendarIds = []
-    #populate calendarIds from the API
+    # populate calendarIds from the API
     for calendar in calendarList['items']:
         calendarIds.append(calendar['id'])
 
-    #print the list of available calendarss
-    i=1
+    # print the list of available calendarss
+    it = 1
     sys.stdout.flush()
     for calendar in calendarList['items']:
-        print(str(i) + ". " + calendar['summary'])
-        i+=1
+        print(str(it) + ". " + calendar['summary'])
+        it += 1
     userInput = 0
 
     print("Choose a calendar to show events from it")
-    #let the user choose the calendar
+    # let the user choose the calendar
     while True:
         try:
             userInput = int(input())
         except ValueError:
             userInput = 0
         finally:
-            if not(1 <= userInput <= i-1):
-                print("The input must be a number!! between 1 and ", i-1)
-            if userInput != 0 and 1 <= userInput <= i-1:
+            if not (1 <= userInput <= it - 1):
+                print("The input must be a number!! between 1 and ", it - 1)
+            if userInput != 0 and 1 <= userInput <= it - 1:
                 break
 
-    #add time for printing purposes
+    # add time
+    #different time format for different things
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    tomorrow = datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+    tomorrow = tomorrow.isoformat() + 'Z'
 
-    #get the events and print them
-    events = service.events().list(calendarId=calendarIds[userInput-1], timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
+    # get the events and print them
+    first10Events = service.events().list(calendarId=calendarIds[userInput - 1], timeMin=now,
+                                   maxResults=10, singleEvents=True,
+                                   orderBy='startTime').execute()
 
-    try:
-        errorBlock = events['items'][0]['id']
-        if events['items']:
-            for event in events['items']:
-                print(event['summary'] + " -----", str(event['start'].get('dateTime')))
-    except:
-        print("This calendar has no events!")
+    #the problem is that today and tomorrow are not of time type
+    weeklyEvents = service.events().list(calendarId=calendarIds[userInput - 1],
+                                         timeMin=now, timeMax=tomorrow,
+                                         singleEvents=True, orderBy='startTime'
+                                         ).execute()
+
+    print("Do you want to see the events for the next day or the first 10 events?")
+    print("1. Events for today")
+    print("2. first 10 events")
+    inp = input()
+    if int(inp) == 1:
+        try:
+            if weeklyEvents['items']:
+                for event in weeklyEvents['items']:
+                    print(event['summary'] + " -----", str(event['start'].get('dateTime')))
+        except:
+            print("This calendar has no events!")
+    if int(inp) == 2:
+        try:
+            if first10Events['items']:
+                for event in first10Events['items']:
+                    print(event['summary'] + " -----", str(event['start'].get('dateTime')))
+        except:
+            print("This calendar has no events!")
+    if inp is not None:
+        # tryingto do the equivalent of ;
+        # which is do nothing
+        inp = 3
+
+    #let the user see all events from all calendars for today
     print('Press Enter to continue...')
     input()
 
-if __name__ == '__main__':
-    #main()
+def doMain():
     while True:
         os.system('cls')
         print("Do you want to close the app or look at another calendar?")
-        print("1. Look at calendar")
-        print("2. Exit")
+        print("1. Look at calendars..")
+        print("2. Exit..")
         inp = input()
         if int(inp) == 1:
             main()
         if int(inp) == 2:
             break
-    #fix this
-    #it doesn't work correctly
+        if inp is not None:
+            # tryingto do the equivalent of ;
+            # which is do nothing
+            inp = 3
+    # fix this
+    # it doesn't work correctly
     for i in range(3, -1, -1):
         if i != 0:
             print('Closing app in ', i, 'seconds')
         time.sleep(1)
+
+
+if __name__ == '__main__':
+    doMain()
+    #transform datetime in time
+    #now = datetime.datetime.utcnow()  # 'Z' indicates UTC time
+    #tonight = now + datetime.timedelta(hours=24)
+    #print(now)
+    #print(tonight.iso)
+
+
+
